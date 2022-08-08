da7fb4b2a16b56e9e1a88d5e2e8df61aa4a73655
Guanngxu <446678850@qq.com>
2022-08-08 14:12:35 +0800

    vsce

--- dist/extension.js
+++ dist/extension.js
@@ -1,6 +1,1 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	
-/******/ 	
-/******/ })()
-;
+(()=>{"use strict";var e,t={};e=t,Object.defineProperty(e,"__esModule",{value:!0}),e.deactivate=e.activate=void 0,e.activate=function(e){},e.deactivate=function(){},module.exports=t})();
